a9a772d80912ffed75de25beb63b8958283477f4
bizarf <snesmaster40@googlemail.com>
2023-08-06 18:48:57 +0100

    fixed fb redirect url

--- controllers/userController.js
+++ controllers/userController.js
@@ -172,7 +172,7 @@ exports.user_facebook_login_callback_get = asyncHandler((req, res, next) => {
                         expiresIn: "30d",
                     });
                     // res.json({ token });
-                    res.redirect("http://localhost:5173/odin-book-client/");
+                    res.redirect("http://localhost:5173/odin-book-client/#/");
                     return res.cookie("jwt_auth", token, { httpOnly: true });
                 });
             }
